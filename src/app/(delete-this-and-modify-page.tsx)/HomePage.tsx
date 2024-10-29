a7b189e3e6598dfd534cd1b7dfbecbed899a6d67
Siddhartha Maity <siddharthamaity1993@gmail.com>
2024-10-29 22:04:50 +0530

    fix: Logo size updated

--- src/app/(delete-this-and-modify-page.tsx)/HomePage.tsx
+++ src/app/(delete-this-and-modify-page.tsx)/HomePage.tsx
@@ -18,7 +18,7 @@ const HomePage: React.FC = () => {
                     />
                     <h6 className='text-3xl font-bold'>+</h6>
                     {/* prettier-ignore */}
-                    <div className="mr-4 flex items-center space-x-2 lg:mr-6"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="size-8"><rect width="256" height="256" fill="none"></rect><line x1="208" y1="128" x2="128" y2="208" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"></line><line x1="192" y1="40" x2="40" y2="192" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"></line></svg><span className="hidden font-bold lg:inline-block text-lg">shadcn/ui</span></div>
+                    <div className="mr-4 flex items-center space-x-2 lg:mr-6"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="size-10"><rect width="256" height="256" fill="none"></rect><line x1="208" y1="128" x2="128" y2="208" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"></line><line x1="192" y1="40" x2="40" y2="192" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"></line></svg><span className="hidden font-bold lg:inline-block text-2xl">shadcn/ui</span></div>
                 </div>
                 <ol className='list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left'>
                     <li className='mb-2'>
